bface45ecc8b3b6d1fb16ac48ae2f86793ed19ab
root <root@fernie1.(none)>
2012-06-11 04:04:32 +0000

    initial

---
+++ server/lib/routes.js
@@ -0,0 +1,31 @@
+var ENV = process.env['NODE_ENV'] || 'development'
+  , config = require('../config')[ENV]
+  , _ = require('underscore')
+  ;
+
+  var sampleData = {
+    email : 'test@test.com',
+    firstName : 'fern',
+    lastName : 'fernberg'
+  };
+
+module.exports = function (app) {
+
+  app.get('/data/sampledata',function(req,res){
+    res.send(sampleData);
+  });
+
+  // Index Page
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
+
+
+  // All other pages
+  app.get('/:page',function(req,res) {
+
+    // View Partial
+    res.partial(req.params.page);
+  });
+
+}
