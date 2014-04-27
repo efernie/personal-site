460b118dbd65043683581b29a1e70977e3bcd7bb
Eric Fernberg <ericf2023@gmail.com>
2014-04-26 20:28:32 -0400

    hello world

--- server/index.js
+++ server/index.js
@@ -1,40 +1,20 @@
-var express = require('express')
-  , ENV = process.env['NODE_ENV'] || 'development'
-  , config = require('./config')[ENV]
-  , cluster = require('cluster')
-  , gzip = require('connect-gzip')
-  , app = express.createServer()
-  , publicDir = __dirname + '/../client'
-;
+var koa = require('koa'),
+    views = require('co-views'),
+    path = require('path'),
+    app = koa(),
+    render = views(path.resolve(__dirname,  '../', 'client/views'), { ext: 'jade' });
 
+app.use( function *( next ) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
 
-app.set('views', __dirname + '/views')
-  .set('view options', { 'layout': false, pretty: true })
-  .set('view engine', 'jade');
+// response
+app.use( function *() {
+  this.body = yield render('index');
+});
 
-
-app.use(express.bodyParser())
-   .use(express.cookieParser())
-   .use(express.favicon())
-   .use(gzip.gzip({ flags: '--best' }))
-;
-
-app.use(express.static(__dirname + '/../client'));
-
-app.use(app.router);
-
-require('./lib')(app);
-
-if (cluster.isMaster) {
-  for (var i = 0; i < 1; i++) {
-    cluster.fork();
-  }
-  cluster.on('death', function(worker) {
-    console.log('worker ' + worker.pid + ' died');
-  });
-} else {
-  app.listen(config.port, function () {
-    var addr = app.address();
-    console.log(('   app listening on http://' + addr.address + ':' + addr.port));
-  });
-}
+app.listen(3000);
+console.log('App listening on port 3000, env:', app.env);
