5a220862831399d23ac157787c31daa1fbc813ff
Eric Fernberg <ericf2023@gmail.com>
2014-04-26 23:05:01 -0400

    base js and scss

---
+++ client/js/app.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function ( load ) {
+  window.removeEventListener('load', load, false);
+  console.log('Window Ready');
+});
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 var koa = require('koa'),
     views = require('co-views'),
+    serve = require('koa-common').static,
     path = require('path'),
     router = require('koa-router'),
     app = koa(),
     config = require('./config')[app.env],
     render = views(path.resolve(__dirname,  '../', 'client/views'), { ext: 'jade' });
 
+app.use(serve( path.resolve(__dirname,  '../', 'client/') ));
 app.use(router(app));
 
 app.use( function *( next ) {
@@ -13,10 +15,16 @@ app.use( function *( next ) {
   yield next;
   var ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
+
 });
 
 app.get('/', function *(next) {
-  this.body = yield render('index');
+  this.locals = {
+    env: app.env,
+    host: 'http://' + this.req.headers.host.slice( 0, this.req.headers.host.indexOf(':') ) + ':35729/livereload.js?snipver=1'
+  };
+
+  this.body = yield render('index', this.locals);
 });
 
 app.listen(config.port);
