e5d55893a233069e9523201ee6e2bcca603e13c3
Eric Fernberg <ericf2023@gmail.com>
2014-04-26 21:23:37 -0400

    add some base stuffs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var koa = require('koa'),
     views = require('co-views'),
     path = require('path'),
+    router = require('koa-router'),
     app = koa(),
     render = views(path.resolve(__dirname,  '../', 'client/views'), { ext: 'jade' }),
     port = 3000;
@@ -9,6 +10,8 @@ if ( app.env === 'production' ) {
   port = 80;
 }
 
+app.use(router(app));
+
 app.use( function *( next ) {
   var start = new Date;
   yield next;
@@ -16,7 +19,7 @@ app.use( function *( next ) {
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
-app.use( function *() {
+app.get('/', function *(next) {
   this.body = yield render('index');
 });
 
